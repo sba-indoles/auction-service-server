74a31f09168592744985f78a9720fc0ee7780469
jay-so <merry9504@naver.com>
2024-11-10 23:36:23 +0900

    feat: 경매 취소, 생성의 정보를 담고 있는 DTO 구현

---
+++ src/main/java/org/indoles/autionserviceserver/core/auction/dto/CancelAuctionCommand.java
@@ -0,0 +1,25 @@
+package org.indoles.autionserviceserver.core.auction.dto;
+
+import java.time.LocalDateTime;
+
+import static org.indoles.autionserviceserver.core.auction.domain.validate.ValidateAuction.validateNotNull;
+
+/**
+ * 경매 취소을 위한 DTO
+ *
+ * @param requestTime 요청 시간
+ * @param auctionId   취소할 경매 ID
+ */
+
+public record CancelAuctionCommand(
+        LocalDateTime requestTime,
+        long auctionId
+) {
+    public CancelAuctionCommand {
+        validateNotNull(requestTime, "requestTime");
+    }
+
+    public void validate() {
+        validateNotNull(requestTime, "requestTime");
+    }
+}
