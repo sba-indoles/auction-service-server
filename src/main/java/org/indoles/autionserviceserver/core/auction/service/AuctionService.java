72b62ede4a884e68560c95358c2f993eb948e634
jay-so <merry9504@naver.com>
2024-11-13 00:22:26 +0900

    refactor: WebConfig 리팩토링

--- src/main/java/org/indoles/autionserviceserver/core/auction/service/AuctionService.java
+++ src/main/java/org/indoles/autionserviceserver/core/auction/service/AuctionService.java
@@ -6,8 +6,7 @@ import org.indoles.autionserviceserver.core.auction.domain.Auction;
 import org.indoles.autionserviceserver.core.auction.dto.AuctionSearchCondition;
 import org.indoles.autionserviceserver.core.auction.dto.*;
 import org.indoles.autionserviceserver.core.auction.entity.AuctionEntity;
-import org.indoles.autionserviceserver.core.auction.entity.exception.AuctionException;
-import org.indoles.autionserviceserver.core.auction.repository.AuctionRepository;
+import org.indoles.autionserviceserver.core.auction.infra.AuctionRepository;
 import org.indoles.autionserviceserver.core.member.dto.response.SignInInfo;
 import org.indoles.autionserviceserver.core.member.entity.enums.Role;
 import org.indoles.autionserviceserver.global.util.Mapper;
@@ -17,219 +16,218 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.indoles.autionserviceserver.core.auction.entity.exception.AuctionExceptionCode.*;
 
-@Slf4j
-@Service
-@Transactional(readOnly = true)
-@RequiredArgsConstructor
-public class AuctionService {
-
-    private final AuctionRepository auctionRepository;
-
-    /**
-     * 경매에 상품을 등록하는 서비스 로직
-     *
-     * @param sellerInfo
-     * @param createAuctionCommand
-     */
-
-    @Transactional
-    public void createAuction(SignInInfo sellerInfo, CreateAuctionCommand createAuctionCommand) {
-        try {
-            createAuctionCommand.validate();
-
-            Auction auction = Auction.builder()
-                    .sellerId(sellerInfo.id())
-                    .productName(createAuctionCommand.productName())
-                    .currentPrice(createAuctionCommand.originPrice())
-                    .originPrice(createAuctionCommand.originPrice())
-                    .currentStock(createAuctionCommand.stock())
-                    .originStock(createAuctionCommand.stock())
-                    .maximumPurchaseLimitCount(createAuctionCommand.maximumPurchaseLimitCount())
-                    .pricePolicy(createAuctionCommand.pricePolicy())
-                    .variationDuration(createAuctionCommand.variationDuration())
-                    .startedAt(createAuctionCommand.startedAt())
-                    .finishedAt(createAuctionCommand.finishedAt())
-                    .isShowStock(createAuctionCommand.isShowStock())
-                    .build();
-
-            AuctionEntity auctionEntity = Auction.toEntity(auction);
-            auctionRepository.save(auctionEntity);
-        } catch (Exception e) {
-            log.error("경매 물품 생성 중 오류 발생", e);
-            throw e;
-        }
-    }
-
-    /**
-     * 경매 시작 전, 등록한 경매에 대해서 취소하는 서비스 로직
-     *
-     * @param signInInfo           경매를 취소하려는 사용자 정보
-     * @param cancelAuctionCommand 취소할 경매 정보
-     */
-
-    @Transactional
-    public void cancelAuction(SignInInfo signInInfo, CancelAuctionCommand cancelAuctionCommand) {
-        if (!signInInfo.isType(Role.SELLER)) {
-            throw new AuctionException(UNAUTHORIZED_SELLER);
-        }
-
-        Auction auction = findAuctionObject(cancelAuctionCommand.auctionId()).toDomain();
-
-        if (!auction.isSeller(signInInfo.id())) {
-            throw new AuctionException(AUCTION_NOT_FOUND);
-        }
-
-        if (!auction.currentStatus(cancelAuctionCommand.requestTime()).isWaiting()) {
-            throw new AuctionException(CANNOT_CANCEL_AUCTION);
-        }
-
-        AuctionEntity auctionEntity = Auction.toEntity(auction);
-        auctionRepository.deleteById(auctionEntity.getId());
-    }
-
-    public AuctionEntity findAuctionObject(Long auctionId) {
-        return auctionRepository.findById(auctionId)
-                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
-    }
-
-    /**
-     * 경매 상품에 대한 입찰(구매)을 진행하는 서비스 로직
-     *
-     * @param auctionId   경매번호
-     * @param price       구매를 원하는 가격
-     * @param quantity    수량
-     * @param requestTime 요청 시간
-     */
-
-    @Transactional
-    public void submitPurchase(Long auctionId, Long price, Long quantity, LocalDateTime requestTime) {
-        Auction auction = findAuctionObject(auctionId).toDomain();
-        auction.submit(price, quantity, requestTime);
-        AuctionEntity auctionEntity = Auction.toEntity(auction);
-        auctionRepository.save(auctionEntity);
-    }
-
-    /**
-     * 경매 상품에 대한 입찰(구매)을 취소하는 서비스 로직
-     *
-     * @param auctionId 경매번호
-     * @param quantity  수량
-     */
-
-    @Transactional
-    public void cancelPurchase(Long auctionId, Long quantity) {
-        LocalDateTime updatedAt = getUpdatedAtForAuction(auctionId);
-        Auction auction = findAuctionObjectForUpdate(auctionId, updatedAt).toDomain();
-        auction.refundStock(quantity);
-
-        AuctionEntity auctionEntity = Auction.toEntity(auction);
-        auctionRepository.save(auctionEntity);
-    }
-
-    private AuctionEntity findAuctionObjectForUpdate(Long auctionId, LocalDateTime updatedAt) {
-        return auctionRepository.findByIdAndUpdatedAt(auctionId, updatedAt)
-                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
-    }
-
-    private LocalDateTime getUpdatedAtForAuction(Long auctionId) {
-        AuctionEntity auctionEntity = auctionRepository.findById(auctionId)
-                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
-        return auctionEntity.getUpdatedAt();
-    }
-
-    /**
-     * 하나의 경매 물품에 대한 상세 정보를 조회하는 서비스 로직
-     *
-     * @param auctionId 경매번호
-     */
-
-    public AuctionInfo getAuction(Long auctionId) {
-        AuctionEntity auctionEntity = auctionRepository.findById(auctionId)
-                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
-
-        Auction auction = auctionEntity.toDomain();
-
-        return Mapper.convertToAuctionInfo(auction);
-    }
-
-    /**
-     * 판매한 경매에 대해 조회하는 서비스 로직(판매자용)
-     *
-     * @param sellerInfo
-     * @param auctionId  경매 ID
-     *                   return 판매자용 경매 정보
-     */
-
-    public SellerAuctionInfo getSellerAuction(SignInInfo sellerInfo, Long auctionId) {
-        if (!sellerInfo.isType(Role.SELLER)) {
-            throw new AuctionException(UNAUTHORIZED_SELLER);
-        }
-
-        AuctionEntity auctionEntity = auctionRepository.findById(auctionId)
-                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
-
-        Auction auction = auctionEntity.toDomain();
-
-        return Mapper.convertToSellerAuctionInfo(auction);
-    }
-
-    /**
-     * 구매한 경매에 대해 조회하는 서비스 로직(구매자용)
-     *
-     * @param auctionId return 구매자용 경매 정보
-     */
-
-    public BuyerAuctionInfo getBuyerAuction(Long auctionId) {
-        AuctionEntity auctionEntity = auctionRepository.findById(auctionId)
-                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
-
-        Auction auction = auctionEntity.toDomain();
-
-        return Mapper.convertToBuyerAuctionInfo(auction);
-    }
-
-    /**
-     * 경매 목록을 조회하는 서비스 로직(판매자용)
-     *
-     * @param condition return 판매자용 경매 정보
-     */
-
-    public List<SellerAuctionSimpleInfo> getSellerAuctionSimpleInfos(SellerAuctionSearchCondition condition) {
-        return auctionRepository.findAllBySellerId(condition.sellerId(), condition.getPageable())
-                .stream()
-                .map(AuctionEntity::toDomain)
-                .map(auction -> new SellerAuctionSimpleInfo(
-                        auction.getId(),
-                        auction.getProductName(),
-                        auction.getOriginPrice(),
-                        auction.getCurrentPrice(),
-                        auction.getOriginStock(),
-                        auction.getCurrentStock(),
-                        auction.getStartedAt(),
-                        auction.getFinishedAt()
-                ))
-                .toList();
-    }
-
-    /**
-     * 경매 목록을 조회하는 서비스 로직(구매자용)
-     *
-     * @param auctionCondition return 구매자용 경매 정보
-     */
-
-    public List<BuyerAuctionSimpleInfo> getBuyerAuctionSimpleInfos(AuctionSearchCondition auctionCondition) {
-        return auctionRepository.findAll(auctionCondition.getPageable())
-                .stream()
-                .map(AuctionEntity::toDomain)
-                .map(auction -> new BuyerAuctionSimpleInfo(
-                        auction.getId(),
-                        auction.getProductName(),
-                        auction.getCurrentPrice(),
-                        auction.getStartedAt(),
-                        auction.getFinishedAt()
-                ))
-                .toList();
-    }
-}
+//@Slf4j
+//@Service
+//@Transactional(readOnly = true)
+//@RequiredArgsConstructor
+//public class AuctionService {
+//
+//    private final AuctionRepository auctionRepository;
+//
+//    /**
+//     * 경매에 상품을 등록하는 서비스 로직
+//     *
+//     * @param sellerInfo
+//     * @param createAuctionCommand
+//     */
+//
+//    @Transactional
+//    public void createAuction(SignInInfo sellerInfo, CreateAuctionCommand createAuctionCommand) {
+//        try {
+//            createAuctionCommand.validate();
+//
+//            Auction auction = Auction.builder()
+//                    .sellerId(sellerInfo.id())
+//                    .productName(createAuctionCommand.productName())
+//                    .currentPrice(createAuctionCommand.originPrice())
+//                    .originPrice(createAuctionCommand.originPrice())
+//                    .currentStock(createAuctionCommand.stock())
+//                    .originStock(createAuctionCommand.stock())
+//                    .maximumPurchaseLimitCount(createAuctionCommand.maximumPurchaseLimitCount())
+//                    .pricePolicy(createAuctionCommand.pricePolicy())
+//                    .variationDuration(createAuctionCommand.variationDuration())
+//                    .startedAt(createAuctionCommand.startedAt())
+//                    .finishedAt(createAuctionCommand.finishedAt())
+//                    .isShowStock(createAuctionCommand.isShowStock())
+//                    .build();
+//
+//            AuctionEntity auctionEntity = Auction.toEntity(auction);
+//            auctionRepository.save(auctionEntity);
+//        } catch (Exception e) {
+//            log.error("경매 물품 생성 중 오류 발생", e);
+//            throw e;
+//        }
+//    }
+//
+//    /**
+//     * 경매 시작 전, 등록한 경매에 대해서 취소하는 서비스 로직
+//     *
+//     * @param signInInfo           경매를 취소하려는 사용자 정보
+//     * @param cancelAuctionCommand 취소할 경매 정보
+//     */
+//
+//    @Transactional
+//    public void cancelAuction(SignInInfo signInInfo, CancelAuctionCommand cancelAuctionCommand) {
+//        if (!signInInfo.isType(Role.SELLER)) {
+//            throw new AuctionException(UNAUTHORIZED_SELLER);
+//        }
+//
+//        Auction auction = findAuctionObject(cancelAuctionCommand.auctionId()).toDomain();
+//
+//        if (!auction.isSeller(signInInfo.id())) {
+//            throw new AuctionException(AUCTION_NOT_FOUND);
+//        }
+//
+//        if (!auction.currentStatus(cancelAuctionCommand.requestTime()).isWaiting()) {
+//            throw new AuctionException(CANNOT_CANCEL_AUCTION);
+//        }
+//
+//        AuctionEntity auctionEntity = Auction.toEntity(auction);
+//        auctionRepository.deleteById(auctionEntity.getId());
+//    }
+//
+//    public AuctionEntity findAuctionObject(Long auctionId) {
+//        return auctionRepository.findById(auctionId)
+//                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
+//    }
+//
+//    /**
+//     * 경매 상품에 대한 입찰(구매)을 진행하는 서비스 로직
+//     *
+//     * @param auctionId   경매번호
+//     * @param price       구매를 원하는 가격
+//     * @param quantity    수량
+//     * @param requestTime 요청 시간
+//     */
+//
+//    @Transactional
+//    public void submitPurchase(Long auctionId, Long price, Long quantity, LocalDateTime requestTime) {
+//        Auction auction = findAuctionObject(auctionId).toDomain();
+//        auction.submit(price, quantity, requestTime);
+//        AuctionEntity auctionEntity = Auction.toEntity(auction);
+//        auctionRepository.save(auctionEntity);
+//    }
+//
+//    /**
+//     * 경매 상품에 대한 입찰(구매)을 취소하는 서비스 로직
+//     *
+//     * @param auctionId 경매번호
+//     * @param quantity  수량
+//     */
+//
+//    @Transactional
+//    public void cancelPurchase(Long auctionId, Long quantity) {
+//        LocalDateTime updatedAt = getUpdatedAtForAuction(auctionId);
+//        Auction auction = findAuctionObjectForUpdate(auctionId, updatedAt).toDomain();
+//        auction.refundStock(quantity);
+//
+//        AuctionEntity auctionEntity = Auction.toEntity(auction);
+//        auctionRepository.save(auctionEntity);
+//    }
+//
+//    private AuctionEntity findAuctionObjectForUpdate(Long auctionId, LocalDateTime updatedAt) {
+//        return auctionRepository.findByIdAndUpdatedAt(auctionId, updatedAt)
+//                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
+//    }
+//
+//    private LocalDateTime getUpdatedAtForAuction(Long auctionId) {
+//        AuctionEntity auctionEntity = auctionRepository.findById(auctionId)
+//                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
+//        return auctionEntity.getUpdatedAt();
+//    }
+//
+//    /**
+//     * 하나의 경매 물품에 대한 상세 정보를 조회하는 서비스 로직
+//     *
+//     * @param auctionId 경매번호
+//     */
+//
+//    public AuctionInfo getAuction(Long auctionId) {
+//        AuctionEntity auctionEntity = auctionRepository.findById(auctionId)
+//                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
+//
+//        Auction auction = auctionEntity.toDomain();
+//
+//        return Mapper.convertToAuctionInfo(auction);
+//    }
+//
+//    /**
+//     * 판매한 경매에 대해 조회하는 서비스 로직(판매자용)
+//     *
+//     * @param sellerInfo
+//     * @param auctionId  경매 ID
+//     *                   return 판매자용 경매 정보
+//     */
+//
+//    public SellerAuctionInfo getSellerAuction(SignInInfo sellerInfo, Long auctionId) {
+//        if (!sellerInfo.isType(Role.SELLER)) {
+//            throw new AuctionException(UNAUTHORIZED_SELLER);
+//        }
+//
+//        AuctionEntity auctionEntity = auctionRepository.findById(auctionId)
+//                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
+//
+//        Auction auction = auctionEntity.toDomain();
+//
+//        return Mapper.convertToSellerAuctionInfo(auction);
+//    }
+//
+//    /**
+//     * 구매한 경매에 대해 조회하는 서비스 로직(구매자용)
+//     *
+//     * @param auctionId return 구매자용 경매 정보
+//     */
+//
+//    public BuyerAuctionInfo getBuyerAuction(Long auctionId) {
+//        AuctionEntity auctionEntity = auctionRepository.findById(auctionId)
+//                .orElseThrow(() -> new AuctionException(AUCTION_NOT_FOUND));
+//
+//        Auction auction = auctionEntity.toDomain();
+//
+//        return Mapper.convertToBuyerAuctionInfo(auction);
+//    }
+//
+//    /**
+//     * 경매 목록을 조회하는 서비스 로직(판매자용)
+//     *
+//     * @param condition return 판매자용 경매 정보
+//     */
+//
+//    public List<SellerAuctionSimpleInfo> getSellerAuctionSimpleInfos(SellerAuctionSearchCondition condition) {
+//        return auctionRepository.findAllBySellerId(condition.sellerId(), condition.getPageable())
+//                .stream()
+//                .map(AuctionEntity::toDomain)
+//                .map(auction -> new SellerAuctionSimpleInfo(
+//                        auction.getId(),
+//                        auction.getProductName(),
+//                        auction.getOriginPrice(),
+//                        auction.getCurrentPrice(),
+//                        auction.getOriginStock(),
+//                        auction.getCurrentStock(),
+//                        auction.getStartedAt(),
+//                        auction.getFinishedAt()
+//                ))
+//                .toList();
+//    }
+//
+//    /**
+//     * 경매 목록을 조회하는 서비스 로직(구매자용)
+//     *
+//     * @param auctionCondition return 구매자용 경매 정보
+//     */
+//
+//    public List<BuyerAuctionSimpleInfo> getBuyerAuctionSimpleInfos(AuctionSearchCondition auctionCondition) {
+//        return auctionRepository.findAll(auctionCondition.getPageable())
+//                .stream()
+//                .map(AuctionEntity::toDomain)
+//                .map(auction -> new BuyerAuctionSimpleInfo(
+//                        auction.getId(),
+//                        auction.getProductName(),
+//                        auction.getCurrentPrice(),
+//                        auction.getStartedAt(),
+//                        auction.getFinishedAt()
+//                ))
+//                .toList();
+//    }
+//}
--- src/main/java/org/indoles/autionserviceserver/global/config/WebConfig.java
+++ src/main/java/org/indoles/autionserviceserver/global/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.indoles.autionserviceserver.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.indoles.autionserviceserver.core.auction.controller.currentTime.CurrentTimeArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,6 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final AuthenticationInterceptor authenticationInterceptor;
     private final AuthenticationArgumentResolver authenticationArgumentResolver;
+    private final CurrentTimeArgumentResolver currentTimeArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -24,6 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authenticationArgumentResolver);
+        resolvers.add(currentTimeArgumentResolver);
     }
 }
 
