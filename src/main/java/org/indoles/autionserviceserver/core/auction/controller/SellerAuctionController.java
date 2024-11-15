94255ef6b379606630297779b069ec07f982b221
jay-so <merry9504@naver.com>
2024-11-15 20:50:03 +0900

    refactor: 판매자 컨트롤러 리팩토링

--- src/main/java/org/indoles/autionserviceserver/core/auction/controller/SellerAuctionController.java
+++ src/main/java/org/indoles/autionserviceserver/core/auction/controller/SellerAuctionController.java
@@ -1,28 +1,30 @@
 package org.indoles.autionserviceserver.core.auction.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.indoles.autionserviceserver.core.auction.controller.currentTime.CurrentTime;
 import org.indoles.autionserviceserver.core.auction.controller.interfaces.SellerOnly;
 import org.indoles.autionserviceserver.core.auction.dto.SignInInfo;
 import org.indoles.autionserviceserver.core.auction.dto.*;
 import org.indoles.autionserviceserver.core.auction.service.AuctionService;
+import org.indoles.autionserviceserver.global.exception.AuthorizationException;
 import org.indoles.autionserviceserver.global.exception.ErrorCode;
-import org.indoles.autionserviceserver.global.exception.InfraStructureException;
+import org.indoles.autionserviceserver.global.util.JwtTokenProvider;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/auctions")
 @RequiredArgsConstructor
 public class SellerAuctionController {
 
     private final AuctionService auctionService;
-    private final ObjectMapper objectMapper;
-
+    private final JwtTokenProvider jwtTokenProvider;
 
     /**
      * 경매 등록 API(판매자 전용)
@@ -31,27 +33,38 @@ public class SellerAuctionController {
     @SellerOnly
     @PostMapping
     public ResponseEntity<Void> createAuction(
-            @RequestHeader("X-SignIn-Info") String sellerInfo,
+            @RequestHeader("Authorization") String authorizationHeader,
             @RequestBody CreateAuctionCommand request,
             @CurrentTime LocalDateTime now
     ) {
 
-        SignInInfo signInInfo = convertToSignInInfo(sellerInfo);
+        String token = authorizationHeader.substring(7);
 
-        CreateAuctionCommand command = new CreateAuctionCommand(
-                request.productName(),
-                request.originPrice(),
-                request.stock(),
-                request.maximumPurchaseLimitCount(),
-                request.pricePolicy(),
-                request.variationDuration(),
-                now,
-                request.startedAt(),
-                request.finishedAt(),
-                request.isShowStock()
-        );
-        auctionService.createAuction(signInInfo, command);
-        return ResponseEntity.ok().build();
+        if (jwtTokenProvider.validateToken(token)) {
+            try {
+                SignInInfo signInInfo = jwtTokenProvider.getSignInInfoFromToken(token);
+                CreateAuctionCommand command = new CreateAuctionCommand(
+                        request.productName(),
+                        request.originPrice(),
+                        request.stock(),
+                        request.maximumPurchaseLimitCount(),
+                        request.pricePolicy(),
+                        request.variationDuration(),
+                        now,
+                        request.startedAt(),
+                        request.finishedAt(),
+                        request.isShowStock()
+                );
+                auctionService.createAuction(signInInfo, command);
+                return ResponseEntity.ok().build();
+            } catch (Exception e) {
+                log.error("Error during chargePoint: {}", e.getMessage());
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+        } else {
+            log.error("Unauthorized: JWT validation failed");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
     /**
@@ -60,14 +73,26 @@ public class SellerAuctionController {
     @SellerOnly
     @DeleteMapping("/{auctionId}")
     public void cancelAuction(
-            @RequestHeader("X-SignIn-Info") String sellerInfo,
+            @RequestHeader("Authorization") String authorizationHeader,
             @PathVariable("auctionId") Long auctionId,
             @CurrentTime LocalDateTime now
     ) {
 
-        SignInInfo signInInfo = convertToSignInInfo(sellerInfo);
-        CancelAuctionCommand command = new CancelAuctionCommand(now, auctionId);
-        auctionService.cancelAuction(signInInfo, command);
+        String token = authorizationHeader.substring(7);
+
+        if (jwtTokenProvider.validateToken(token)) {
+            try {
+                SignInInfo signInInfo = jwtTokenProvider.getSignInInfoFromToken(token);
+                CancelAuctionCommand command = new CancelAuctionCommand(now, auctionId);
+                auctionService.cancelAuction(signInInfo, command);
+            } catch (Exception e) {
+                log.error("Error during chargePoint: {}", e.getMessage());
+                throw new AuthorizationException("Unauthorized: JWT validation failed", ErrorCode.A001);
+            }
+        } else {
+            log.error("Unauthorized: JWT validation failed");
+            throw new AuthorizationException("Unauthorized: JWT validation failed", ErrorCode.A001);
+        }
     }
 
     /**
@@ -76,15 +101,27 @@ public class SellerAuctionController {
     @SellerOnly
     @GetMapping("/seller")
     public ResponseEntity<List<SellerAuctionSimpleInfo>> getSellerAuctions(
-            @RequestHeader("X-SignIn-Info") String sellerInfo,
+            @RequestHeader("Authorization") String authorizationHeader,
             @RequestParam(name = "offset") int offset,
             @RequestParam(name = "size") int size
     ) {
-        SignInInfo signInInfo = convertToSignInInfo(sellerInfo);
 
-        SellerAuctionSearchCondition condition = new SellerAuctionSearchCondition(signInInfo.id(), offset, size);
-        List<SellerAuctionSimpleInfo> infos = auctionService.getSellerAuctionSimpleInfos(condition);
-        return ResponseEntity.ok(infos);
+        String token = authorizationHeader.substring(7);
+
+        if (jwtTokenProvider.validateToken(token)) {
+            try {
+                SignInInfo signInInfo = jwtTokenProvider.getSignInInfoFromToken(token);
+                SellerAuctionSearchCondition condition = new SellerAuctionSearchCondition(signInInfo.id(), offset, size);
+                List<SellerAuctionSimpleInfo> infos = auctionService.getSellerAuctionSimpleInfos(condition);
+                return ResponseEntity.ok(infos);
+            } catch (Exception e) {
+                log.error("Error during chargePoint: {}", e.getMessage());
+                throw new AuthorizationException("Unauthorized: JWT validation failed", ErrorCode.A001);
+            }
+        } else {
+            log.error("Unauthorized: JWT validation failed");
+            throw new AuthorizationException("Unauthorized: JWT validation failed", ErrorCode.A001);
+        }
     }
 
     /**
@@ -94,21 +131,24 @@ public class SellerAuctionController {
     @SellerOnly
     @GetMapping("/{auctionId}/seller")
     public ResponseEntity<SellerAuctionInfo> getSellerAuction(
-            @RequestHeader("X-SignIn-Info") String sellerInfo,
+            @RequestHeader("Authorization") String authorizationHeader,
             @PathVariable("auctionId") Long auctionId
     ) {
 
-        SignInInfo signInInfo = convertToSignInInfo(sellerInfo);
+        String token = authorizationHeader.substring(7);
 
-        SellerAuctionInfo info = auctionService.getSellerAuction(signInInfo.id(), auctionId);
-        return ResponseEntity.ok(info);
-    }
-
-    private SignInInfo convertToSignInInfo(String signInInfoString) {
-        try {
-            return objectMapper.readValue(signInInfoString, SignInInfo.class);
-        } catch (Exception e) {
-            throw new InfraStructureException("SignInfo 변환 실패" + e, ErrorCode.A031);
+        if (jwtTokenProvider.validateToken(token)) {
+            try {
+                SignInInfo signInInfo = jwtTokenProvider.getSignInInfoFromToken(token);
+                SellerAuctionInfo info = auctionService.getSellerAuction(signInInfo, auctionId);
+                return ResponseEntity.ok(info);
+            } catch (Exception e) {
+                log.error("Error during chargePoint: {}", e.getMessage());
+                throw new AuthorizationException("Unauthorized: JWT validation failed", ErrorCode.A001);
+            }
+        } else {
+            log.error("Unauthorized: JWT validation failed");
+            throw new AuthorizationException("Unauthorized: JWT validation failed", ErrorCode.A001);
         }
     }
 }
