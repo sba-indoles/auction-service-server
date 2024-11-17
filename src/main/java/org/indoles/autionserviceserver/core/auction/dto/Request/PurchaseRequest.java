3fea462658c526119d6c2e6f187ccf3bc24eb634
jay-so <merry9504@naver.com>
2024-11-17 20:23:32 +0900

    refactor: PurchaseRequest Dto 리팩토링

--- src/main/java/org/indoles/autionserviceserver/core/auction/dto/Request/PurchaseRequest.java
+++ src/main/java/org/indoles/autionserviceserver/core/auction/dto/Request/PurchaseRequest.java
@@ -3,6 +3,9 @@ package org.indoles.autionserviceserver.core.auction.dto.Request;
 import org.indoles.autionserviceserver.global.exception.BadRequestException;
 import org.indoles.autionserviceserver.global.exception.ErrorCode;
 
+import static org.indoles.autionserviceserver.core.auction.dto.validateDto.ValidateAuctionDto.validatePrice;
+import static org.indoles.autionserviceserver.core.auction.dto.validateDto.ValidateAuctionDto.validateQuantity;
+
 public record PurchaseRequest(
         long price,
         long quantity
@@ -10,18 +13,6 @@ public record PurchaseRequest(
     public PurchaseRequest {
         validatePrice(price);
         validateQuantity(quantity);
-    }
-
-    private void validatePrice(long price) {
-        if (price < 0) {
-            throw new BadRequestException("경매 입찰 요청 가격은 음수일 수 없습니다. 요청가격: " + price, ErrorCode.A026);
-        }
-    }
-
-    private void validateQuantity(long quantity) {
-        if (quantity <= 0) {
-            throw new BadRequestException("경매 입찰 요청 수량은 0보다 커야합니다. 요청수량: " + quantity, ErrorCode.A027);
-        }
     }
 }
 
--- src/main/java/org/indoles/autionserviceserver/core/auction/dto/validateDto/ValidateAuctionDto.java
+++ src/main/java/org/indoles/autionserviceserver/core/auction/dto/validateDto/ValidateAuctionDto.java
@@ -108,6 +108,22 @@ public class ValidateAuctionDto {
     }
 
     /**
+     * 입찰 DTO에서 사용되는 유효성 검사
+     * @param price
+     */
+
+    public static void validatePrice(long price) {
+        if (price < 0) {
+            throw new BadRequestException("경매 입찰 요청 가격은 음수일 수 없습니다. 요청가격: " + price, ErrorCode.A026);
+        }
+    }
+
+    public static void validateQuantity(long quantity) {
+        if (quantity <= 0) {
+            throw new BadRequestException("경매 입찰 요청 수량은 0보다 커야합니다. 요청수량: " + quantity, ErrorCode.A027);
+        }
+    }
+    /**
      * 경매 DTO에서 공통적으로 사용되는 유효성 검사
      *
      * @param value
