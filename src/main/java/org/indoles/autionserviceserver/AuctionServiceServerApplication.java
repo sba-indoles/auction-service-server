34f26280139cf4966dee645f2e3c5bacc10ff689
jay-so <merry9504@naver.com>
2024-11-22 15:06:10 +0900

    chore: MemberFeignClient, ReceiptFeighClient URL 변수 등록

--- src/main/java/org/indoles/autionserviceserver/AuctionServiceServerApplication.java
+++ src/main/java/org/indoles/autionserviceserver/AuctionServiceServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 public class AuctionServiceServerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AuctionServiceServerApplication.class, args);
     }
